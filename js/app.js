70dcea7fff4c39ba886f92c37bc1896951ae22d5
Joe Jackson <joe.jackson@scimedsolutions.com>
2013-11-11 02:00:56 -0500

    Add Creation

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ App = Ember.Application.create();
 App.Router.map(function() {
   this.resource('widgets', { path: '/widgets' }, function(){
     this.resource('widget', { path: ':widget_id' });
+    this.route('new', { path: 'new' });
   });
 });
 
@@ -12,11 +13,24 @@ App.WidgetsRoute = Ember.Route.extend({
          }
 });
 
+App.WidgetsNewRoute = Ember.Route.extend({
+  model: function() {
+           return this.store.createRecord('widget')
+         },
+  actions: {
+             create: function(model) {
+                       model.save();
+                       this.transitionTo('widgets');
+                     }
+           }
+
+});
+
 App.Widget = DS.Model.extend({
   name: DS.attr(),
   description: DS.attr()
 });
 
 App.ApplicationAdapter = DS.RESTAdapter.extend({
-  host: "http://jackson.local:3000"
+  host: "http://jackson.local:3000",
 });
